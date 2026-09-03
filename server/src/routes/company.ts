e1326877a9d219c586e2303b4e798bee213f2e30
Artur Samoilov <artsamoilov@gmail.com>
2026-09-03 15:50:38 +0200

    feat: add client data fetching

--- server/src/routes/company.ts
+++ server/src/routes/company.ts
@@ -7,6 +7,8 @@ const companyData = companyDataRaw as IRawNode;
 export const companyRouter = Router();
 
 companyRouter.get("/company", (req, res) => {
+    res.set("Cache-Control", "no-store");
+
     // Timeout to simulate a slow API call for loading state demonstration
     setTimeout(() => {
         res.json(companyData);
